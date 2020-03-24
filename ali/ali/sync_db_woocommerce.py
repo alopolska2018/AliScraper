9e900319644708a9e6fba94bf035c542dedd6258
michał_kulisiewicz <michalkulisiewicz@gmail.com>
2020-03-24 15:04:28 +0100

    Fixed issues with creating variants, changed url for requests

--- ali/ali/sync_db_woocommerce.py
+++ ali/ali/sync_db_woocommerce.py
@@ -15,15 +15,13 @@ class SyncDbWoocommerce():
         db = self.client['ali']
         self.collection = db['ali_tb']
         self.wcapi = API(
-            url="http://93.104.210.240/nowy/wordpress",
-            consumer_key="ck_21ad21d1bac02e0abacdac7db56cf0cfe98267d3",
-            consumer_secret="cs_2f17a73fb6f528a01a417e68580a44a769ad9fbd",
+            url="http://server764802.nazwa.pl/wordpress",
+            consumer_key="ck_28c44a0b2526deeac2791f629e51a394f281b1ad",
+            consumer_secret="cs_30e4817bbba122d6cf28c694d14f58f4c061bdbe",
             wp_api=True,
             version="wc/v3"
         )
         self.run()
-
-
 
     def run(self):
         self.check_unadded_products_in_woocommerce()
@@ -42,55 +40,166 @@ class SyncDbWoocommerce():
             #TODO Create mapping between ali_categories and woo
             #categories = product['categories']
             images = product['data']['images']
-            response = self.create_main_product_woo(name, regular_price, description)
-            woo_id = self.get_woo_id()
-            self.add_main_woocommerce_id_to_database(woo_id, db_id)
+            # attributes = []
             variants = self.get_all_variants_of_product(db_id)
+            attributes_properties = self.get_main_product_attributes(variants)
+
+
+            response = self.create_main_product_woo(name, regular_price, description, attributes_properties)
+            woo_id = self.get_woo_id(response)
+            self.add_main_woocommerce_id_to_database(woo_id, db_id)
+
+            self.get_all_variants_attributes(variants)
+            self.create_variants_woo(woo_id, db_id, variants)
+            print('aa')
 
     def add_main_woocommerce_id_to_database(self, woo_id, db_id):
         self.collection.find_one_and_update({'_id': ObjectId(db_id)}, {'$set': {'woocommerce_id': woo_id}}, upsert=True)
 
-    def create_main_product_woo(self, name, regular_price, description):
+    def create_main_product_woo(self, name, regular_price, description, attributes_properties):
         data = {}
+
         data['name'] = name
-        data['type'] = 'simple'
+        data['type'] = 'variable'
         data['regular_price'] = regular_price
         data['description'] = description
+        data['attributes'] = attributes_properties
         # data['categories'] = categories
         # data['images'] = images
         response = self.wcapi.post("products", data).json()
         return response
 
     def test(self):
-        # self.add_main_woocommerce_id_to_database(23, '5e471581beac03e2549cfc30')
-        # variants = self.get_all_variants_of_product('5e471581beac03e2549cfc30')
-        # self.create_variants_woo('23', variants)
-        # self.add_variant_woocommerce_id_to_database('22', '5e4abb16beac03e2549d0658')
-        self.run()
+        variants = self.get_all_variants_of_product('5e70cb5d3364872c3599445a')
+        attributes_properties_list = self.get_main_product_attributes(variants)
+        self.create_variants_woo('5e70cb5d3364872c3599445a', '231', variants)
+
     def get_all_variants_of_product(self, db_id):
         item = self.collection.find_one({'_id': ObjectId(db_id)})
         return item['data']['products']
 
-    def add_variant_to_woo(self, woo_id, sku, dict):
+    def get_all_variants_attributes(self, variants):
+        available_attributes = {}
+        colour_list = []
+        size_list = []
+        length_list = []
+        available_attributes['colour'] = colour_list
+        available_attributes['size'] = size_list
+        available_attributes['length'] = length_list
+        for key, val in variants.items():
+            if 'colour' in val:
+                colour = val['colour']
+                if colour not in colour_list:
+                    colour_list.append(colour)
+
+            if 'size' in val:
+                size = val['size']
+                if size not in size_list:
+                    size_list.append(size)
+
+            if 'length' in val:
+                length = val['length']
+                if length not in length_list:
+                    length_list.append(length)
+
+        return available_attributes
+
+    def get_variant_attributes(self, val):
+        attributes = []
+
+        if 'colour' in val:
+            colour = val['colour']
+
+            attribute = {}
+            attribute['id'] = 24
+            attribute['option'] = colour
+            attributes.append(attribute)
+
+        if 'size' in val:
+            size = val['size']
+
+            attribute = {}
+            attribute['id'] = 6
+            attribute['option'] = size
+            attributes.append(attribute)
+
+        if 'length' in val:
+            length = val['length']
+
+            attribute = {}
+            attribute['id'] = 8
+            attribute['option'] = length
+            attributes.append(attribute)
+
+        return attributes
+
+
+    def create_variant_dict(self, sku, variation, attributes):
         data = {}
         image = {}
-        image['src'] = dict['img_url']
+        image['src'] = variation['img_url']
         #TODO Calculate regular price
-        data['regular_price'] = dict['price']
-        data['stock_quantity'] = dict['qty']
+        data['regular_price'] = variation['price']
+        data['manage_stock'] = True
+        data['stock_quantity'] = variation['qty']
         data['sku'] = sku
         data['image'] = image
-        #TODO add attributes
+        data['attributes'] = attributes
 
-        return self.wcapi.post("products/{}/variations".format(woo_id), data).json()
+        # response = self.wcapi.post("products/{}/variations".format(woo_id), data).json()
+        return data
 
     def add_variant_woocommerce_id_to_database(self, woo_id, db_id, sku):
         self.collection.find_one_and_update({'_id': ObjectId(db_id)}, {'$set': {'data.products.{}.woocommerce_variant_id'.format(sku): woo_id}}, upsert=True)
 
-    def create_variants_woo(self, woo_id, db_id, variants):
-        for key, value in variants.items():
-            woo_id = self.add_variant_to_woo(woo_id, key, value)
-            self.add_variant_woocommerce_id_to_database(woo_id, db_id, key)
+    def add_variant_to_woo_batch(self, variants, woo_id):
+        data = {}
+        data['create'] = variants
+        response = self.wcapi.post("products/{}/variations/batch".format(woo_id), data).json()
+        print(response)
 
+    def create_variants_woo(self, woo_id, db_id, variants):
+        final_variants_list = []
+
+        for key, value in variants.items():
+            attributes = self.get_variant_attributes(value)
+            data = self.create_variant_dict(key, value, attributes)
+            #TODO ADD CREATED VARIANT ID TO DATABASE
+            # self.add_variant_woocommerce_id_to_database(woo_id, db_id, key)
+            final_variants_list.append(data)
+        print(final_variants_list)
+        if len(final_variants_list) > 100:
+            raise Exception('Maximum variants for one product is 100')
+        else:
+            self.add_variant_to_woo_batch(final_variants_list, woo_id)
+
+
+    def get_main_product_attributes(self, variants):
+        attributes_properties_list = []
+        attributes =  self.get_all_variants_attributes(variants)
+        counter = 0
+        #TODO Create a function that will read avaiable attributes from woo and creat not existing
+        for key, val in attributes.items():
+            if val:
+                if key == 'colour':
+                    id = 24
+                elif key == 'length':
+                    id = 8
+                elif key == 'size':
+                    id = 6
+                else:
+                    raise Exception('No attributes available for {}'.format(key))
+
+                attributes_properties_dict = {}
+                attributes_properties_dict['id'] = id
+                # attributes_properties_dict['name'] = name
+                attributes_properties_dict['position'] = counter
+                attributes_properties_dict['visible'] = True
+                attributes_properties_dict['variation'] = True
+                attributes_properties_dict['options'] = val
+                attributes_properties_list.append(attributes_properties_dict)
+                counter += 1
+
+        return attributes_properties_list
 
 a = SyncDbWoocommerce()
